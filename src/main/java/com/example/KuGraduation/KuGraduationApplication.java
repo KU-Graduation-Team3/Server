a5aa3c75986bc10cfe646d7781bbadb23465c941
dlwjddus1112 <dlwjddus1112@naver.com>
2025-10-25 14:12:21 +0900

    [Chore] 도커 파일 작성

---
+++ src/main/java/com/example/KuGraduation/KuGraduationApplication.java
@@ -0,0 +1,13 @@
+package com.example.KuGraduation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuGraduationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuGraduationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KuGraduation/KuGraduationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KuGraduation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuGraduationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
