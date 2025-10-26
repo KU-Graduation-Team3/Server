9f364b70e59961852b8d743a7d1acc107042234c
dlwjddus1112 <dlwjddus1112@naver.com>
2025-10-26 16:22:11 +0900

    [Feat] RestClient 설정

---
+++ src/main/java/com/example/KuGraduation/global/config/RestClientConfig.java
@@ -0,0 +1,27 @@
+package com.example.KuGraduation.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfig {
+
+    @Bean
+    RestClient crawlerClient(@Value("${crawler.base-url}") String baseUrl) {
+        var rf = new SimpleClientHttpRequestFactory();
+        rf.setConnectTimeout(1500);
+        rf.setReadTimeout(4000);
+        return RestClient.builder().baseUrl(baseUrl).requestFactory(rf).build();
+    }
+
+    @Bean
+    RestClient sentimentClient(@Value("${sentiment.base-url}") String baseUrl) {
+        var rf = new SimpleClientHttpRequestFactory();
+        rf.setConnectTimeout(1500);
+        rf.setReadTimeout(6000);
+        return RestClient.builder().baseUrl(baseUrl).requestFactory(rf).build();
+    }
+}
