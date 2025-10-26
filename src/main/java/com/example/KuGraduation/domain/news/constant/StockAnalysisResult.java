6df1bbc4f7845f191b74c0ab584a15e0d9f6829d
dlwjddus1112 <dlwjddus1112@naver.com>
2025-10-26 16:39:21 +0900

    [Chore] 분석 결과 enum 네이밍 수정

--- src/main/java/com/example/KuGraduation/domain/news/constant/StockAnalysisResult.java
+++ src/main/java/com/example/KuGraduation/domain/news/constant/StockAnalysisResult.java
@@ -1,7 +1,7 @@
 package com.example.KuGraduation.domain.news.constant;
 
-public enum Overall { POSITIVE, NEUTRAL, NEGATIVE;
-    public static Overall fromSum(int sum) {
+public enum StockAnalysisResult { POSITIVE, NEUTRAL, NEGATIVE;
+    public static StockAnalysisResult fromSum(int sum) {
         if (sum > 0) return POSITIVE;
         if (sum < 0) return NEGATIVE;
         return NEUTRAL;
